f2042c0cefad92c9dc2289bb566ccad02e81624a
Mohammad Hafijul Islam <hafijul233@gmail.com>
2023-11-12 03:11:08 +0600

    external request logger updated

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Fintech\Bell\Channels\SmsChannel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,8 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return redirect()->route('request-docs.index');
-//    \Illuminate\Support\Facades\Notification::route(SmsChannel::class, '+8801689553434')->notify(new \App\Notifications\TestSMS());
+Route::get('/', function (Request $request) {
+//    return redirect()->route('request-docs.index');
+    dd($_SERVER);
+
+    $response = Http::get('http://devstarter.test/api/core/settings',['test' => '1']);
+    dd($response->body());
+//    \Illuminate\Support\Facades\Notification::route('sms', '+8801689553434')->notify(new \App\Notifications\TestSMS());
 
 });
