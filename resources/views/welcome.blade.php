b206b132e47d3a1732e6a82967f0c7a8afbe534b
Mohammad Hafijul Islam <hafijul233@gmail.com>
2023-12-03 06:39:11 +0600

    currency rate convert method added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,22 @@
-<?php
-phpinfo();
-?>
+@php
+$data1 = [
+    'service_id' => 1,
+    'source_country_id' => 1,
+    'destination_country_id' => 19,
+    'amount' => 1000,
+    'reverse' => false
+];
+
+dump(\Fintech\Business\Facades\Business::currencyRate()->convert($data1));
+
+$data2 = [
+    'service_id' => 1,
+    'source_country_id' => 1,
+    'destination_country_id' => 19,
+    'amount' => 1000,
+    'reverse' => true,
+    'get_only_rate' => true
+];
+
+dd(\Fintech\Business\Facades\Business::currencyRate()->convert($data2));
+@endphp
